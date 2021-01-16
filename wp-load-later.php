b2d497be69476b12675117980aff0183e48d9565
Joseph Scott <joseph@josephscott.org>
2021-01-16 11:43:42 -0700

    Add support for loading JS after the load event fires

--- wp-load-later.php
+++ wp-load-later.php
@@ -8,6 +8,17 @@
 
 class WP_Load_Later {
 	private static $deferred_js = [];
+	private static $after_load_js = [];
+
+	/**
+	 * Call this to load a JS resource after the load event fires
+	 *
+	 * @param string $url
+	 * @param array $attributes key/value pairs
+	 */
+	public static function after_load_js( $url, $attributes = [] ) {
+		self::$after_load_js[ $url ] = $attributes;
+	}
 
 	/**
 	 * Call this to add another JS resource that uses the defer attribute.
@@ -28,6 +39,7 @@ class WP_Load_Later {
 	 */
 	public static function init() {
 		add_action( 'wp_footer', [ 'WP_Load_Later', '_inject_deferred_js' ], 99999 );
+		add_action( 'wp_footer', [ 'WP_Load_Later', '_inject_after_load' ], 99999 );
     }
 
 	/************************/
@@ -46,6 +58,48 @@ class WP_Load_Later {
 			}
 			echo "src='$js'></script>\n"; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 		}
+	}
+
+	/**
+	 * Inject resources to be loaded after the load event fires
+	 */
+	public static function _inject_after_load() {
+		if ( count( self::$after_load_js ) < 1 ) {
+			return;
+		}
+
+		$js = wp_json_encode( self::_escape_all( self::$after_load_js ) );
+
+		$out = <<<HTML
+<script>
+window.addEventListener( 'load', function( event ) {
+	function appendToDoc( element, attributes ) {
+		for ( var attr in attributes ) {
+			try {
+				element.setAttribute( attr, attributes[attr] );
+			} catch ( e ) {
+				console.log( "WP_Load_Later: Invalid attribute", e );
+			}
+		}
+		document.body.appendChild( element );
+	}
+
+	try {
+		var js = JSON.parse( '$js' );
+		for ( var url in js ) {
+			var script = document.createElement( 'script' );
+			script.src = url;
+
+			appendToDoc( script, js[url] );
+		}
+	} catch( e ) {
+		console.log( 'WP_Load_Later: add_after_load failure', e );
+	}
+} );
+</script>
+HTML;
+
+		echo $out; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 	}
 
 	/**
